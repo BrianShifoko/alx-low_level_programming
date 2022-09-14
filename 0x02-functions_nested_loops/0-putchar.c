44647c2b08afb83993e3e7f1687156add0e375ff
BrianShifoko <brianshifoko@gmail.com>
2022-09-14 07:17:14 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <main.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,6 +8,8 @@
  */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+
+	printf("_putchar\n");
+
+	return (0);
 }
