4e74545b13a4ddc1afe785486e148a1211eca003
BrianShifoko <brianshifoko@gmail.com>
2022-09-15 02:55:57 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,5 +10,21 @@
 
 int main(void)
 {
+long i = 0;
+long j = 1;
+int n = 0;
+long fib;
+
+while (n < 50)
+{
+fib = j + i;
+if (n != 49)
+printf("%ld, ", fib);
+else
+printf("%ld\n", fib);
+i = j;
+j = fib;
+n += 1;
+}
 return (0);
 }
