9dcb5b28e42a5d09f8fdaf6bcf864dc4539181dd
BrianShifoko <brianshifoko@gmail.com>
2022-09-14 23:11:38 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN.H
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif/*MAIN_H*/
