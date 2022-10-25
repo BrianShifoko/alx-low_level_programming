1f5500397b209db28c5721d438f4e3c9f1eb67ed
BrianShifoko <brianshifoko@gmail.com>
2022-10-25 10:19:22 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,36 +1,35 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * add_node - function that prints all the elements of a list_t list
- * @head: input header pointer
- * @str: Input string value
- * Return: address of the new element, or NULL if it failed
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
+char *dup;
+int len;
+list_t *new;
+new = malloc(sizeof(list_t));
+if (new == NULL)
 return (NULL);
-new_node->len = _strlen(str);
-new_node->str = strdup(str);
-new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
-/**
- * _strlen - returns length of string
- * @s: character of string
- * Return: length of string
- */
-int _strlen(const char *s)
+dup = strdup(str);
+if (dup == NULL)
 {
-int i;
-while (s[i] != 0)
-i++;
-return (i);
+free(new);
+return (NULL);
+}
+for (len = 0; str[len];)
+len++;
+new->str = dup;
+new->len = len;
+new->next = *head;
+*head = new;
+return (new);
 }
